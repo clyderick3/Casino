850b6eb87b3b05c25f943429277d7f42dfcfde4c
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-05-04 11:34:33 -0400

    engine

--- src/main/java/com/github/zipcodewilmington/casino/games/GameEngine/SlotsEngine.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/GameEngine/SlotsEngine.java
@@ -1,5 +1,6 @@
 package com.github.zipcodewilmington.casino.games.GameEngine;
 
+import com.github.zipcodewilmington.casino.games.TicTacToe.TicTacToePlayer;
 import com.github.zipcodewilmington.casino.games.slots.SlotsGame;
 import com.github.zipcodewilmington.casino.games.slots.SlotsPlayer;
 
@@ -8,6 +9,7 @@ public class SlotsEngine extends GameEngine<SlotsPlayer, SlotsGame>{
 
     @Override
     public void start() {
+        getGame().addPlayer(getPlayers().get(0));
         getGame().play();
     }
 }
